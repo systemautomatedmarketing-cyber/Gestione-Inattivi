f120b30488b605673c36319c3a58ef81bccc7828
systemautomatedmarketing-cyber <systemautomatedmarketing@gmail.com>
2026-02-23 01:10:14 +0100

    Update page.tsx

--- sge-inattivi/src/app/admin/inattivi/page.tsx
+++ sge-inattivi/src/app/admin/inattivi/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 import { useEffect, useMemo, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
